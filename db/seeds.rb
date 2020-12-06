0217e1ada886fb216b1ca839067d5c2f56a461fc
WENWU YAN <968828@gmail.com>
2020-12-06 11:42:52 +0800

    初始化activeadmin配置

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+AdminUser.create!(email: '968828@gmail.com', password: 'careline', password_confirmation: 'careline') if Rails.env.development?
